27a592d4d69d2768e1d1c41b0d694e4ffd97fb05
MARIA EDUARDA ZABEL <maria_e_zabel@estudante.sesisenai.org.br>
2026-03-25 14:45:17 -0300

    MZ feat: implementa nova tela, e organiza paleta de cores

---
+++ app/src/main/java/com/example/api_receitas/features/authentication/ui/AuthenticationScreen.kt
@@ -0,0 +1,12 @@
+package com.example.api_receitas.features.authentication.ui
+
+import androidx.compose.runtime.Composable
+
+class AuthenticationScreen {
+
+    @Composable
+    fun AuthenticationScreen(){
+
+    }
+
+}
