d26b20e6cdffe3aa85ccbfe793ea3a58057ec6ea
KAEL LUIH DE ARAUJO <kael_araujo@estudante.sesisenai.org.br>
2026-03-25 15:40:58 -0300

    Feat: Inicializa a criação das primeiras telas do app

--- app/src/main/java/com/example/api_receitas/MainActivity.kt
+++ app/src/main/java/com/example/api_receitas/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.api_receitas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.api_receitas.features.home.ui.InitialScreenLogo
 import com.example.api_receitas.ui.theme.APIReceitasTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             APIReceitasTheme {
-
+                InitialScreenLogo();
             }
         }
     }
