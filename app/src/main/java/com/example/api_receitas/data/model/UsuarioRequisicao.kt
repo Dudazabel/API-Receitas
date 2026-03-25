3bcb32801faf744f45b81bceb848856ad2f1bc9c
KAEL LUIH DE ARAUJO <kael_araujo@estudante.sesisenai.org.br>
2026-03-25 20:33:31 -0300

    Revert "Merge pull request #16 from Dudazabel/feature/Integracao-api-usuario"
    This reverts commit b1fa0830ff8fe23923597db7bdb9ec79746ee74c, reversing
    changes made to a7b477647217620e1d9ece19b946052f9a4a81d1.

--- app/src/main/java/com/example/api_receitas/data/model/UsuarioRequisicao.kt
+++
@@ -1,9 +0,0 @@
-package com.example.api_receitas.data.model
-
-import android.provider.ContactsContract.CommonDataKinds.Email
-
-data class UsuarioRequisicao(
-    val nome: String,
-    val email: String,
-    val senha: String
-)
--- app/src/main/java/com/example/api_receitas/data/model/UsuarioResposta.kt
+++
@@ -1,5 +0,0 @@
-package com.example.api_receitas.data.model
-
-data class UsuarioResposta(val id: Long,
-                           val nome: String,
-                           val email: String)
