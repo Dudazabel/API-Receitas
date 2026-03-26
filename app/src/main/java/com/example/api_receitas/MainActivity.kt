ffbc75d2e4fc2d40ff4c91fe1f69680e45bfc8c7
MARIA EDUARDA ZABEL <maria_e_zabel@estudante.sesisenai.org.br>
2026-03-26 16:12:02 -0300

    MZ feat: implementa tela para confirmação de cadastro

--- app/src/main/java/com/example/api_receitas/MainActivity.kt
+++ app/src/main/java/com/example/api_receitas/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.example.api_receitas.features.authentication.ui.AuthenticationLogIn
 import com.example.api_receitas.features.authentication.ui.AuthenticationSignIn
+import com.example.api_receitas.features.confirmation.ui.telaConfimacao
 import com.example.api_receitas.features.details.ui.RecipeDetailScreen
 import com.example.api_receitas.ui.theme.APIReceitasTheme
 
@@ -26,14 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-                    RecipeDetailScreen(
-                        receitaId = 1L,
-                        onVoltarClick = {
-
-                            println("Usuário clicou em voltar")
-                        }
-                    )
+                    telaConfimacao()
                 }
             }
         }
